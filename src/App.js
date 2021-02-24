66570ca521375301e75c8ef89f41a614ec6223b6
run-after <run.after@protonmail.com>
2021-02-24 11:19:55 -0600

    Add basic layout for activity grid

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
       <header className="App-header">
         Autobooks
       </header>
+      <div className='activity-grid'>
+        <div className='row'>
+          <div className='start-time'>Start time</div>
+        <div className='end-time'>End time</div>
+        <div className='duration'>Duration</div>
+        <div className='description'>Description</div>
+        </div>
+      </div>
     </div>
   );
 }
